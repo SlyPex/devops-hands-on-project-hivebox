63053f037c9c66c97ad1f04a93ceeeb77b48223b
Slimane MEHARZI <59923212+SlyPex@users.noreply.github.com>
2025-11-02 22:09:27 +0100

    test(config): update mocks to use Path.open()

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,27 +1,31 @@
-"""Unit tests for src/config.py"""
+"""Unit tests for src/config.py."""
+
 import importlib
-from unittest.mock import patch, mock_open
+from pathlib import Path
+from unittest.mock import mock_open, patch
+
 from ..src import config as c
 
 
-@patch("builtins.open", new_callable=mock_open, read_data="v0.0.1")
+@patch.object(Path, "open", new_callable=mock_open, read_data="v0.0.1")
 def test_config_load_version_success(mocked_open):
-    """Unit test for loading config (version) from file"""
+    """Unit test for loading config (version) from file."""
     importlib.reload(c)
     assert c.VERSION == "v0.0.1"
-    mocked_open.assert_called_once_with(file=c.VERSION_FILE, mode="r", encoding="utf-8")
+    mocked_open.assert_called_once_with(encoding="utf-8")
 
-@patch("builtins.open", side_effect=FileNotFoundError('File Not Found'))
+
+@patch.object(Path, "open", side_effect=FileNotFoundError("File Not Found"))
 def test_config_load_version_file_not_found(mocked_open):
-    """Unit test for loading config (version) with FileNotFound exception"""
+    """Unit test for loading config (version) with FileNotFound exception."""
     importlib.reload(c)
     assert c.VERSION == ""
-    mocked_open.assert_called_once_with(file=c.VERSION_FILE, mode="r", encoding="utf-8")
+    mocked_open.assert_called_once_with(encoding="utf-8")
 
-@patch("builtins.open", side_effect=PermissionError('Permission denied'))
+
+@patch.object(Path, "open", side_effect=PermissionError("Permission denied"))
 def test_config_load_version_permission_denied(mocked_open):
-    """Unit test for loading config (version) with FileNotFound exception"""
+    """Unit test for loading config (version) with FileNotFound exception."""
     importlib.reload(c)
     assert c.VERSION == ""
-    mocked_open.assert_called_once_with(file=c.VERSION_FILE, mode="r", encoding="utf-8")
-    
+    mocked_open.assert_called_once_with(encoding="utf-8")
