e831d11adc438fce845a81513fafdd5cb9967370
Slimane MEHARZI <59923212+SlyPex@users.noreply.github.com>
2025-11-02 22:06:49 +0100

    style: apply ruff formatting

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,30 +1,33 @@
-"""Module providing unit tests for the API (src/main.py)"""
-from unittest.mock import patch
-from fastapi.testclient import TestClient
-from fastapi import status
-from ..src import main
+"""Module providing unit tests for the API (src/main.py)."""
 
+from unittest.mock import patch
+
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from ..src import main
 
 client = TestClient(main.app)
 
 
 def test_welcome():
-    """unit test for the welcome endpoint."""
+    """Unit test for the welcome endpoint."""
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Welcome to DevOps Project"}
+
 
 def test_version_success():
     """Unit test for the version endpoint (success)."""
     with patch.object(main, "VERSION", "v0.0.1"):
         response = client.get("/version")
         assert response.status_code == status.HTTP_200_OK
-        assert response.json() == {"version":"v0.0.1"}
+        assert response.json() == {"version": "v0.0.1"}
+
 
 def test_version_when_config_missing():
-    """Unit test for the version endpoint (failure)"""
+    """Unit test for the version endpoint (failure)."""
     with patch.object(main, "VERSION", ""):
         response = client.get("/version")
         assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
         assert response.json() == {"detail": "Service configuration unavailable"}
-    
